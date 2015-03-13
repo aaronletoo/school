2d479c4fae374989af762ba314aa2051db99110a
Aaron <aaronletoo@gmail.com>
2015-03-13 15:57:46 +0800

    建立目录结构

---
+++ management/management-business/management-business-customer/src/main/java/org/whut/management/business/customer/entity/Customer.java
@@ -0,0 +1,7 @@
+package org.whut.management.business.customer.entity;
+
+/**
+ * Created by letoo on 2015/3/13.
+ */
+public class Customer {
+}
---
+++ management/management-business/management-business-customer/src/main/java/org/whut/management/business/customer/service/CustomerService.java
@@ -0,0 +1,7 @@
+package org.whut.management.business.customer.service;
+
+/**
+ * Created by letoo on 2015/3/13.
+ */
+public class CustomerService {
+}
